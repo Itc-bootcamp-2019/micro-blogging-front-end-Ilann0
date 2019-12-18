5c3cf120848a1204c346b1f68cf194aae3eb4054
Ilann0 <ilann.ohayon@gmail.com>
2019-12-19 00:50:42 +0200

    handle unable to post when message has white space only

--- src/components/CreateTweet.jsx
+++ src/components/CreateTweet.jsx
@@ -12,7 +12,7 @@ class CreateTweet extends React.PureComponent {
 
     handleInputChange(e) {
         const string    = e.target.value
-        const stringLen = e.target.value.length
+        const stringLen = e.target.value.replace(/\s/g, '').length
 
         this.setState(() => {
             return (stringLen < 141) && {
@@ -22,53 +22,70 @@ class CreateTweet extends React.PureComponent {
         })
     }
 
-    handlePost(val, parentCallback) {
-        if (val.length >= 1) {
-            this.setState({
-                inputVal: '',
-                inputLen: 0,
-            })
-            parentCallback(val)
-        }
+    handlePost(val, valLen, parentCallback) {
+        if (val.length >= 1 && valLen) {
+			this.setState({
+				inputVal: '',
+				inputLen: 0,
+			});
+			parentCallback(val);
+		}
     }
 
     render() {
         const { inputVal, inputLen } = this.state
         return (
-            <div className="tweet-box input-frame">
-                <textarea
-                    name="tweet"
-                    id="tweet-input"
-                    placeholder="What you have in mind.."
-                    value={inputVal}
-                    onChange={(e) => this.handleInputChange(e)}
-                />
-                <div className="tb-bottom">
-                    <span className={ inputLen > 132 ?
-                        (inputLen > 139 ? 'chars-left red' : 'chars-left orange')
-                        : 'chars-left' }
-                    >
-                        { inputLen }/140
-                    </span>
-                    <TweetManagerContext.Consumer>
-                        {
-                            ({onPost, requestPending}) => {
-                                if (requestPending) {
-                                    return <img className="post-btn" src="https://causematch-production-snpurdytanj1dxi.netdna-ssl.com/wp-content/themes/causematch/resources/images/rdp-bars.gif" alt=""/>
-                                } else {
-                                    return (
-                                        <button
-                                            className={ (inputLen === 0) ? 'post-btn disabled' : 'post-btn' }
-                                            onClick={ () => this.handlePost(inputVal, onPost) }
-                                        >Post</button>
-                                    )
-                                }
-                            }
-                        }
-                    </TweetManagerContext.Consumer>
-                </div>
-            </div>
-        );
+			<div className="tweet-box input-frame">
+				<textarea
+					name="tweet"
+					id="tweet-input"
+					placeholder="What you have in mind.."
+					value={inputVal}
+					onChange={e => this.handleInputChange(e)}
+				/>
+				<div className="tb-bottom">
+					<span
+						className={
+							inputLen > 132
+								? inputLen > 139
+									? 'chars-left red'
+									: 'chars-left orange'
+								: 'chars-left'
+						}
+					>
+						{inputLen}/140
+					</span>
+					<TweetManagerContext.Consumer>
+						{({ onPost, requestPending }) => {
+							if (requestPending) {
+								return (
+									<img
+										className="post-btn"
+										src="https://causematch-production-snpurdytanj1dxi.netdna-ssl.com/wp-content/themes/causematch/resources/images/rdp-bars.gif"
+										alt=""
+									/>
+								);
+							} else {
+								return (
+									<button
+										className={
+											inputLen === 0
+												? 'post-btn disabled'
+												: 'post-btn'
+										}
+										onClick={() =>
+											this.handlePost(inputVal, inputLen, onPost)
+										}
+									>
+										Post
+									</button>
+								);
+							}
+						}}
+					</TweetManagerContext.Consumer>
+				</div>
+			</div>
+		);
     }
 }
 
